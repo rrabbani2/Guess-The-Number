0c3d21f38371ec7a142b8fc3fa526a4a71eea60d
EC2 Default User <ec2-user@ip-172-31-27-154.us-east-2.compute.internal>
2019-08-01 18:36:50 +0000

    stable version 3

--- app/models/model.py
+++ app/models/model.py
@@ -3,12 +3,19 @@ import random
 def number_checker(userguess):
     realnumber = random.randint(1,10)
     if int(userguess) ==  realnumber:
-        return {"win":True,"answer":realnumber}
+        return {"win":True,"answer":realnumber,"hint":"Nothing here"}
+    elif int(userguess)>int(realnumber):
+        return {"win":False,"answer":realnumber, "hint":"Try a lower number"}
     else:
-        return {"win":False,"answer":realnumber}
+        return {"win":False,"answer":realnumber, "hint":"Try a higher number"}
+
+    
        
 def repeatcheck(userguess, realnumber): 
-    if userguess==realnumber:
+    if int(userguess)==int(realnumber):
         return {"win":True,"answer":realnumber}
+    elif int(userguess)>int(realnumber):
+         return {"win":False,"answer":realnumber, "hint":"Try a lower number"}
     else:
-        return {"win":False,"answer":realnumber}
+        return {"win":False,"answer":realnumber,"hint":"Try a higher number"}
+
--- app/routes.py
+++ app/routes.py
@@ -12,13 +12,14 @@ def index():
 def results():
     answer = ""
     answer_dictionary = model.number_checker(request.form['numguesser'])
-    print(answer_dictionary["answer"])
+    print(answer_dictionary)
     if answer_dictionary["win"]:
         answer="Congrats you are the number jedi"
         return render_template("results.html",answer=answer, num=request.form['numguesser'])
     else:
+        hint=(answer_dictionary["hint"])
         answer="Sorry, you didn't get the number :(\nthe correct answer was "+str(answer_dictionary["answer"])
-        return render_template("keepguessing.html",num=request.form['numguesser'], x=answer_dictionary["answer"])
+        return render_template("keepguessing.html",num=request.form['numguesser'], x=answer_dictionary["answer"], hint=hint)
         
         
 @app.route('/keepguessing', methods = ['GET','POST'])
@@ -29,7 +30,9 @@ def guessing():
     if answer_dictionary["win"]:
         answer="Congrats you are the number jedi"
         return render_template("results.html",answer=answer, num=request.form['numguesser'])
-    else :
-        return render_template('keepguessing.html', num=request.form['numguesser'], x=answer_dictionary["answer"])
+    else:
+        hint=(answer_dictionary["hint"])
+        return render_template('keepguessing.html', num=request.form['numguesser'], x=answer_dictionary["answer"], hint=hint)
+        
 
 
