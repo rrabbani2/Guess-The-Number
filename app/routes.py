009de0dbad80152a9f439defc62159dc60d3cd77
EC2 Default User <ec2-user@ip-172-31-27-154.us-east-2.compute.internal>
2019-08-01 14:35:08 +0000

    MVP

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,17 @@ def index():
     return render_template("index.html")
     
     
+@app.route("/results",methods =['GET','POST'])
+def results():
+    answer = ""
+    answer_dictionary = model.number_checker(request.form['numguesser'])
+    if answer_dictionary["win"]:
+        answer="Congrats you are the number master"
+    else:
+        answer="Sorry, you didn't get the number :(\nthe correct answer was "+str(answer_dictionary["answer"])
+    return render_template("results.html",answer=answer, num=request.form['numguesser'])
+    
+
+
+
+
